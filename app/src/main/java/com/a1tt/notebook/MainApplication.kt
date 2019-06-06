5416acc847d069b4b4d7d231ece4a693a4d509a0
a1tt <a1tt@rambler.ru>
2019-06-06 22:58:31 +0300

    added crashlytics

--- app/src/main/java/com/a1tt/notebook/MainApplication.kt
+++ app/src/main/java/com/a1tt/notebook/MainApplication.kt
@@ -3,9 +3,12 @@ package com.a1tt.notebook
 import android.app.Application
 import android.graphics.Bitmap
 import android.os.CountDownTimer
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import io.fabric.sdk.android.Fabric
 
 class MainApplication :Application(){
     override fun onCreate() {
@@ -13,6 +16,9 @@ class MainApplication :Application(){
         instance = this
         AppCenter.start(this, "bcc4667b-07d3-4cbf-bcf3-a306e5e48f97", Analytics::class.java, Crashes::class.java)
         Analytics.trackEvent("test")
+        val crashlytics = Crashlytics.Builder().core(CrashlyticsCore.Builder().disabled(false).build()).build()
+        Fabric.with(this, crashlytics)
+        Crashlytics.log("test1")
     }
 
     companion object {
