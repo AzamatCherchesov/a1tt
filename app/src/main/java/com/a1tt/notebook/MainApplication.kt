71d4ffc298399ad1013c991c24802acd48d845c5
a1tt <a1tt@rambler.ru>
2019-06-06 21:54:59 +0300

    added appCenter support

--- app/src/main/java/com/a1tt/notebook/MainApplication.kt
+++ app/src/main/java/com/a1tt/notebook/MainApplication.kt
@@ -3,11 +3,16 @@ package com.a1tt.notebook
 import android.app.Application
 import android.graphics.Bitmap
 import android.os.CountDownTimer
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainApplication :Application(){
     override fun onCreate() {
         super.onCreate()
         instance = this
+        AppCenter.start(this, "bcc4667b-07d3-4cbf-bcf3-a306e5e48f97", Analytics::class.java, Crashes::class.java)
+        Analytics.trackEvent("test")
     }
 
     companion object {
